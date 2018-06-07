ee92825eaad25c171640e40c2e976744f34c98f4
KyleWang <wk52525@126.com>
2018-06-07 15:44:29 +0800

    init project

---
+++ src/main/java/top/kylewang/redismq/RedismqApplication.java
@@ -0,0 +1,12 @@
+package top.kylewang.redismq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedismqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedismqApplication.class, args);
+	}
+}
